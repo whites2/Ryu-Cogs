e9d7e0df6a7cf548c5412667ff29107522327ff6
whites2 <whites2@g.cofc.edu>
2017-08-23 14:43:50 -0400

    changed 'text' to 'flavor'

--- hearthstone/hearthstone.py
+++ hearthstone/hearthstone.py
@@ -15,7 +15,7 @@ class Hearthstone:
         r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format(name), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
         
         await self.bot.say(r.json()[0]['img'])
-        await self.bot.say(r.json()[0]['text'])
+        await self.bot.say(r.json()[0]['flavor'])
 
     @commands.command()
     async def gcard(self, name):
@@ -24,7 +24,7 @@ class Hearthstone:
         r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format(name), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
         
         await self.bot.say(r.json()[0]['imgGold'])
-        await self.bot.say(r.json()[0]['text'])
+        await self.bot.say(r.json()[0]['flavor'])
 
 def setup(bot):
     bot.add_cog(Hearthstone(bot))
