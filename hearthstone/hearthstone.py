3789c626ce16636bb566445b10849f8ebd066e2f
Scott White <whites2@g.cofc.edu>
2017-08-23 14:29:01 -0400

    added hearthstone card command

---
+++ hearthstone/hearthstone.py
@@ -0,0 +1,26 @@
+import discord
+import requests
+from discord.ext import commands
+
+class Hearthstone:
+        """Cog for searching Hearthstone cards"""
+        
+        def __init__(self, bot):
+            self.bot = bot
+        
+        @commands.command()
+        async def card(self, name):
+            """Searches for a regular Hearthstone card"""
+
+            r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format(name), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
+            
+            await self.bot.say(r.json()[0]['img'])
+            await self.bot.say(r.json()[0]['text'])
+
+        async def gcard(self, name):
+            """Searches for a gold Hearthstone card"""
+
+            r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format(name), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
+            
+            await self.bot.say(r.json()[0]['imgGold'])
+            await self.bot.say(r.json()[0]['text'])
