9ba4f029413f3560de7efb001915cbd8e081f9d5
Scott White <whites2@g.cofc.edu>
2018-04-03 17:41:38 -0400

    updated hearthstone.py

--- hearthstone/hearthstone.py
+++ hearthstone/hearthstone.py
@@ -14,11 +14,14 @@ class Hearthstone:
 
         r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format(name), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
         
+        print(r)
         print(r.json())
         print(name)
-        
-        await self.bot.say(r.json()[0]['img'])
-        await self.bot.say(r.json()[0]['flavor'])
+        if r == '<Response [404]>':
+            await self.bot.say('Card ' + name + ' not found.'
+        else:
+            await self.bot.say(r.json()[0]['img'])
+            await self.bot.say(r.json()[0]['flavor'])
 
     @commands.command()
     async def gcard(self, name):
