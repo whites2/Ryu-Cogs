911a612ac0844cf87aa9383f88b0c4fa9ae7bd5b
Scott White <whites2@g.cofc.edu>
2018-04-03 17:37:00 -0400

    updated hearthstone.py

--- hearthstone/hearthstone.py
+++ hearthstone/hearthstone.py
@@ -12,7 +12,7 @@ class Hearthstone:
     async def card(self, name):
         """Searches for a regular Hearthstone card"""
 
-        r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format("/"" + name + "/""), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
+        r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format(name), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
         
         await self.bot.say(r.json()[0]['img'])
         await self.bot.say(r.json()[0]['flavor'])
@@ -21,7 +21,7 @@ class Hearthstone:
     async def gcard(self, name):
         """Searches for a gold Hearthstone card"""
 
-        r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format("/"" + name + "/""), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
+        r = requests.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/{}'.format(name), headers={'X-Mashape-Key':'sly1A6Ur3tmshrDtRbWe4q738Afxp1cnkhajsnWqVf9HMJ7ZOJ'}) 
         
         await self.bot.say(r.json()[0]['imgGold'])
         await self.bot.say(r.json()[0]['flavor'])
